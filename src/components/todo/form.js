da99745fd298abe26a1776500fa9633f8bc01795
jonathon lee <jonnyleealas@gmail.com>
2020-12-06 15:06:20 -0600

    got values from handleChange

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,27 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 
+
+
+
 function MainForm(){
+
+    const [itemObject, setItemObject] = useState({})
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(itemObject)
+        e.target.reset()
+    }
+
+    const handleChange = (e) =>{
+    setItemObject({...itemObject, [e.target.name]: e.target.value})
+    console.log(itemObject)
+    }
 
     return(
         <>
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group controlId="item">
                 <Form.Label>To Do Item</Form.Label>
-                <Form.Control type="text" placeholder="Add a task..." />
+                <Form.Control name="text" type="text" onChange={handleChange} placeholder="Add a task..." />
             </Form.Group>
 
             <Form.Group controlId="assignee">
-                <Form.Label>Assigne To:</Form.Label>
-                <Form.Control type="text" placeholder="Assigned To" />
+                <Form.Label>Assign To:</Form.Label>
+                <Form.Control name="assignee" type="text" onChange={handleChange} placeholder="Assigned To" />
             </Form.Group>
             
             <Form>
             <Form.Group controlId="formBasicRange">
                 <Form.Label>Range</Form.Label>
-                <Form.Control type="range" min="1" max="5" />
+                <Form.Control name="difficulty" onChange={handleChange} type="range" min="1" max="5" />
             </Form.Group>
             </Form>
             <Button variant="primary" type="submit">
